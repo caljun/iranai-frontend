812fae7f0e7e0416ea8768d5ece65b3731c13ac9
caljun <calderonjunya0602@gmail.com>
2025-04-06 21:57:02 +0900

    Update profile.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    localStorage.clear();
     const gallery = document.querySelector(".gallery");
     const profileUrl = document.getElementById("profileUrl");
     const userUrl = document.getElementById("userUrl");
